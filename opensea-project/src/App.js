a3f786e927905f60c4a7410c305b7b036c19f5cd
Robin Hanjun Kwon <khj02021@gmail.com>
2022-04-11 08:58:27 +0900

    Set-up App.js

--- opensea-project/src/App.js
+++ opensea-project/src/App.js
@@ -1,9 +1,47 @@
+// Import packages
+import {useState, useEffect} from 'react';
+import Web3 from 'web3';
+
+// Import CSS
 import './App.css';
 
+// Import pages
+import Home from './pages/Home/Home';
+import TokenList from './pages/TokenList/TokenList';
+
+// Import components
+import NavBar from './components/NavBar/NavBar';
+
 function App() {
+  const [web3, setWeb3] = useState();
+  const [account, setAccount] = useState();
+
+  const connectWallet = async () => {
+	const accounts = await window.ethereum.request({
+	  method: 'eth_requestAccounts'
+	});
+
+	setAccount(accounts[0]);
+  }
+
+  useEffect(() => {
+	if(window.ethereum !== undefined) {
+	  try {
+		const web3Obj = new Web3(window.ethereum);
+		setWeb3(web3Obj);
+	  } catch(err) {
+		console.log(err);
+	  }
+	}
+  }, []);
+
   return (
 	<div className='App'>
-
+	  <NavBar />
+	  <button className='metaConnect' onClick={() => {connectWallet();}}>Connect metamask</button>
+	  <div className='userInfo'>Address: {account}</div>
+	  <Home />
+	  <TokenList web3={web3} account={account} />
 	</div>
   );
 }
---
+++ opensea-project/src/pages/TokenList/TokenList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TokenList = () => {
+  return (
+	<div>This is Token List!!</div>
+  );
+}
+
+export default TokenList;
