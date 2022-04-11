cb0ea6e72d1885e26f3ede8651894e66bf1f116a
Woohyun Yang <woohyunyang@Woohyunui-MacBookAir.local>
2022-04-11 22:41:45 +0900

    Add contact, description

--- opensea-project/src/pages/Home/Home.js
+++ opensea-project/src/pages/Home/Home.js
@@ -27,16 +27,20 @@ const Home = () => {
           Kwon
           <div>Team Leader</div>
           <ul>Project Leading</ul>
-          <ul>Minting Page</ul>
+          <ul>Minting Page Implement</ul>
           {/* <img src="깃허브 로고 이미지" alt=""></img> */}
         </div>
         <div className="teammembers">
           Baek
           <div>Team Member</div>
+          <ul>Tokenlist Page Implement</ul>
+          <ul>Page Routing</ul>
         </div>
         <div className="teammembers">
           Yang
           <div>Team Member</div>
+          <ul>Home Page Implement</ul>
+          <ul>Deploy Smart Contract</ul>
         </div>
       </div>
       <div className="partner">
@@ -46,6 +50,32 @@ const Home = () => {
       </div>
       <div className="contact">
         <h2>Contact</h2>
+        GitHub
+        <a
+          href="https://github.com/codestates/beb-03-GroundY"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+            alt=""
+            height="40"
+            width="40"
+          ></img>
+        </a>
+        Notion
+        <a
+          href="https://codestates.notion.site/1-7ce6f34197a14d99b71f49c7071d1ef8"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png?20200221181224"
+            alt=""
+            height="40"
+            width="40"
+          ></img>
+        </a>
       </div>
     </div>
   );
