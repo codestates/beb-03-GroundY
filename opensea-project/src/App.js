1a05b8b6d74bd9941c4586e33df8df75d9e4fd8b
BEB03 <cpt9136@gmail.com>
2022-04-11 15:30:26 +0900

    save tokenlist [temp]

--- opensea-project/src/App.js
+++ opensea-project/src/App.js
@@ -11,11 +11,15 @@ import "./App.css";
 // Import pages
 import Home from "./pages/Home/Home";
 import TokenList from "./pages/TokenList/TokenList";
+import erc721Abi from "./erc721Abi";
 
 // Import components
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
+  const [erc721list, setErc721list] = useState([]);
+  const [newErc721addr, setNewErc721Addr] = useState();
+
   const [web3, setWeb3] = useState();
   const [account, setAccount] = useState();
 
@@ -38,6 +42,28 @@ function App() {
     }
   }, []);
 
+  const addNewErc721Token = async () => {
+    const tokenContract = await new web3.eth.Contract(erc721Abi, newErc721addr);
+    console.log(tokenContract);
+    const name = await tokenContract.methods.name().call();
+    const symbol = await tokenContract.methods.symbol().call();
+    const totalSupply = await tokenContract.methods.totalSupply().call();
+
+    let arr = [];
+    for (let i = 1; i <= totalSupply; i++) {
+      arr.push(i);
+    }
+    for (let tokenId of arr) {
+      let tokenOwner = await tokenContract.methods.ownerOf(tokenId).call();
+      if (String(tokenOwner).toLowerCase() === account) {
+        let tokenURI = await tokenContract.methods.tokenURI(tokenId).call();
+        setErc721list((prevState) => {
+          return [...prevState, { name, symbol, tokenId, tokenURI }];
+        });
+      }
+    }
+  };
+
   return (
     <div className="App">
       <NavBar />
@@ -57,7 +83,17 @@ function App() {
       </button>
       <div className="userInfo">Address: {account}</div>
       <Home />
-      <TokenList web3={web3} account={account} />
+      <div className="newErc721">
+        <input
+          type="text"
+          onChange={(e) => {
+            setNewErc721Addr(e.target.value);
+            // 입력받을 때마다 newErc721addr 갱신
+          }}
+        ></input>
+        <button onClick={addNewErc721Token}>add new erc721</button>
+      </div>
+      <TokenList web3={web3} account={account} erc721list={erc721list} />
     </div>
   );
 }
--- opensea-project/src/pages/TokenList/TokenList.js
+++ opensea-project/src/pages/TokenList/TokenList.js
@@ -1,8 +1,50 @@
-import React from 'react';
+// import packages
+import React from "react";
+import { useState } from "react";
+import Web3 from "web3";
 
-const TokenList = () => {
+// import pages
+import Erc721 from "./Erc721";
+import erc721Abi from "../../erc721Abi";
+
+function TokenList({ web3, account, erc721list }) {
+  // const [newErc721addr, setNewErc721Addr] = useState();
+  // const [erc721list, setErc721list] = useState([]);
+
+  // const addNewErc721Token = async ({ web3, account }) => {
+  //   const tokenContract = await new web3.eth.Contract(erc721Abi, newErc721addr);
+  //   const name = await tokenContract.methods.name().call();
+  //   const symbol = await tokenContract.methods.symbol().call();
+  //   const totalSupply = await tokenContract.methods.totalSupply().call();
+
+  //   let arr = [];
+  //   for (let i = 1; i <= totalSupply; i++) {
+  //     arr.push(i);
+  //   }
+  //   for (let tokenId of arr) {
+  //     let tokenOwner = await tokenContract.methods.ownerOf(tokenId).call();
+  //     if (String(tokenOwner).toLowerCase() === account) {
+  //       let tokenURI = await tokenContract.methods.tokenURI(tokenId).call();
+  //       setErc721list((prevState) => {
+  //         return [...prevState, { name, symbol, tokenId, tokenURI }];
+  //       });
+  //     }
+  //   }
+  // };
+
   return (
-	<div>This is Token List!!</div>
+    // <div className="newErc721">
+    //   <input
+    //     type="text"
+    //     onChange={(e) => {
+    //       setNewErc721Addr(e.target.value);
+    //     }}
+    //   ></input>
+    //   <button onClick={addNewErc721Token}>add new erc721</button>
+    <div className="tokenlist">
+      <Erc721 web3={web3} account={account} erc721list={erc721list} />
+    </div>
+    // </div>
   );
 }
 
