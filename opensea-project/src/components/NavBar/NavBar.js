fd5734dd36dca7cc199ec2bfcb2ab46e536ed82d
Robin Hanjun Kwon <khj02021@gmail.com>
2022-04-14 00:19:53 +0900

    Add CSS to navbar component

--- opensea-project/src/components/NavBar/NavBar.js
+++ opensea-project/src/components/NavBar/NavBar.js
@@ -1,31 +1,87 @@
+// import packages
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
 
-const NavBar = ({account, setAccount}) => {
-  const connectWallet = async () => {
-	const accounts = await window.ethereum.request({
-	  method: 'eth_requestAccounts'
-	});
+const Container = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background: pink;
+`;
 
-	setAccount(accounts[0]);
+const Logo = styled.div`
+  display: flex;
+  flex-direction: flex-start;
+  height: 4em;
+  line-height: 4em;
+  padding: 0 10px 0 10px;
+`;
+
+const Items = styled.div`
+  line-height: 4em;
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+`;
+
+const Item = styled.div`
+  padding: 0 10px 0 10px;
+  &:hover {
+	background: yellow;
   }
+`;
 
+const Button = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  font-size: 1.2rem;
+  padding: 0;
+  &:hover {
+	color: grey;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  font-size: 1.2rem;
+  &:hover {
+	color: grey;
+  }
+`;
+
+const NavBar = ({account, connectWallet, isLoggedIn}) => {
   return (
-    <div>
-      <Link to="/">
-        <button>home</button>
-      </Link>
-      <Link to="/tokenlist">
-        <button>TokenList</button>
-      </Link>
-      <Link to="/mint">
-        <button>Mint</button>
-      </Link>
-	  <div>
-		<button onClick={() => {connectWallet();}}>Log in with Metamask</button>
-		<span>Address: {account}</span>
-	  </div>
-    </div>
+	<Container>
+	  <Logo>
+		<StyledLink to='/'>Ground Y</StyledLink>
+	  </Logo>
+	  <Items>
+		<Item>
+		  <StyledLink to="/">
+			Home
+		  </StyledLink>
+		</Item>
+		<Item>
+		  <StyledLink to="/tokenlist">
+			Token List
+		  </StyledLink>
+		</Item>
+		<Item>
+		  <StyledLink to="/mint">
+			Mint
+		  </StyledLink>
+		</Item>
+		<Item>
+		  <Button onClick={() => {connectWallet();}}>{isLoggedIn ? 'Logged in' : 'Log in with Metamask'}</Button>
+		</Item>
+	  </Items>
+	</Container>
   );
 };
 
