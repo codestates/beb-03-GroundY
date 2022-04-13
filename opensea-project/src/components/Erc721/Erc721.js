e30ccce949e4f990b5ba546bb442a1f8bed4785b
BEB03 <cpt9136@gmail.com>
2022-04-13 15:29:58 +0900

    fix tokenlist bug

--- opensea-project/src/components/Erc721/Erc721.js
+++ opensea-project/src/components/Erc721/Erc721.js
@@ -13,13 +13,13 @@ const Erc721list = styled.div`
 `;
 
 const Erc721token = styled.div`
-  width: 30%;
+  width: 100%;
   overflow: auto;
   margin: 1%;
   border: 1px solid green;
 `;
 
-function Erc721({ web3, account, erc721list, newErc721addr }) {
+function SeeToken({ web3, account, token, newErc721addr }) {
   const [showModal, setShowModal] = useState(false);
 
   const openModal = () => {
@@ -32,27 +32,40 @@ function Erc721({ web3, account, erc721list, newErc721addr }) {
 
   return (
     <Erc721list>
+      <Erc721token>
+        <span className="name">{token.name}</span>(
+        <span className="symbol">{token.symbol}</span>)
+        <div className="nft">#{token.tokenId}</div>
+        <img
+          alt=""
+          className="nftimg"
+          src={token.NFTdata.image}
+          onClick={openModal}
+        />
+        <Modal
+          showModal={showModal}
+          closeModal={closeModal}
+          web3={web3}
+          account={account}
+          token={token}
+          newErc721addr={newErc721addr}
+        ></Modal>
+      </Erc721token>
+    </Erc721list>
+  );
+}
+
+function Erc721({ web3, account, erc721list, newErc721addr }) {
+  return (
+    <Erc721list>
       {erc721list.map((token) => {
         return (
-          <Erc721token>
-            <span className="name">{token.name}</span>(
-            <span className="symbol">{token.symbol}</span>)
-            <div className="nft">#{token.tokenId}</div>
-            <img
-              alt=""
-              className="nftimg"
-              src={token.NFTdata.image}
-              onClick={openModal}
-            />
-            <Modal
-              showModal={showModal}
-              closeModal={closeModal}
-              web3={web3}
-              account={account}
-              token={token}
-              newErc721addr={newErc721addr}
-            ></Modal>
-          </Erc721token>
+          <SeeToken
+            token={token}
+            web3={web3}
+            account={account}
+            newErc721addr={newErc721addr}
+          ></SeeToken>
         );
       })}
     </Erc721list>
