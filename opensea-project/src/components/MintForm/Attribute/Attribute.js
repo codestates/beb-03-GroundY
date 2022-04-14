75aca7d84484583e4d907365b1cb8cac9c8f9181
Robin Hanjun Kwon <khj02021@gmail.com>
2022-04-14 16:58:48 +0900

    Work with CSS

--- opensea-project/src/components/MintForm/Attribute/Attribute.js
+++ opensea-project/src/components/MintForm/Attribute/Attribute.js
@@ -1,10 +1,24 @@
+// import packages
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+`;
+
+const Button = styled.button.attrs(props => ({
+  onClick: props.onClick,
+}))`
+  
+`
 
 const Attribute = ({attributeValues, handleAttribute, addAttribute, removeAttribute}) => {
   return (
-	<div>
+	<>
 	  {attributeValues.map((el, idx) => (
-		<div key={idx} >
+		<Container key={idx} >
 		  <label>Trait Type: </label>
 		  <input type='text' name='trait_type' value={el.trait_type || ''} onChange={(e) => {handleAttribute(e, idx);}} />
 		  <label>Value: </label>
@@ -14,10 +28,10 @@ const Attribute = ({attributeValues, handleAttribute, addAttribute, removeAttrib
 			  <button onClick={() => {removeAttribute(idx);}}>Remove attribute</button> :
 			  null
 		  }
-		</div>
+		</Container>
 	  ))}
 	  <button onClick={() => {addAttribute();}} >Add attribute</button>
-	</div>
+	</>
   );
 }
 
--- opensea-project/src/components/MintForm/MintForm.js
+++ opensea-project/src/components/MintForm/MintForm.js
@@ -1,26 +1,63 @@
+// import packages
 import React from 'react';
+import styled from 'styled-components';
 
 // import components
 import Attribute from './Attribute/Attribute';
 
+// create styled-components
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  border: solid black 1px;
+  border-radius: 10px;
+  margin: 10px;
+  padding: 10px;
+`;
+
+const Label = styled.label`
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+	'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+	sans-serif;
+`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+`;
+
+const Input = styled.input.attrs(props => ({
+  type: props.type,
+  name: props.name,
+  value: props.value || '',
+  onChange: props.onChange
+}))`
+
+`;
+
 const MintForm = ({submit, retrieveImage, attributeValues, handleAttribute, addAttribute, removeAttribute, data, handleData}) => {
   return (
-	<div>
-	  <div>
-		<label>Image: </label>
-		<input type='file' name='image' onChange={retrieveImage} />
-	  </div>
-	  <div>
-		<label>Name: </label>
-		<input type='text' name='name' value={data.name} onChange={handleData} />
-	  </div>
-	  <div>
-		<label>Description: </label>
-		<input type='text' name='description' value={data.description} onChange={handleData} />
-	  </div>
-	  <Attribute attributeValues={attributeValues} handleAttribute={handleAttribute} addAttribute={addAttribute} removeAttribute={removeAttribute} />
+	<>
+	  <Container>
+		<ItemContainer>
+		  <Label>Image: </Label>
+		  <input type='file' name='image' onChange={retrieveImage} />
+		</ItemContainer>
+		<ItemContainer>
+		  <Label>Name: </Label>
+		  <Input type='text' name='name' value={data.name} onChange={handleData} />
+		</ItemContainer>
+		<ItemContainer>
+		  <Label>Description: </Label>
+		  <Input type='text' name='description' value={data.description} onChange={handleData} />
+		</ItemContainer>
+		<Attribute attributeValues={attributeValues} handleAttribute={handleAttribute} addAttribute={addAttribute} removeAttribute={removeAttribute} />
+	  </Container>
 	  <button onClick={() => {submit();}}>Submit</button>
-	</div>
+	</>
   );
 }
 
